fa7781ff9bff75e2d2601eccea45393a8b990440
thonghp <thonghoangpham123@gmail.com>
2024-06-23 02:52:39 +0700

    refactor code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { OrderComponent } from './order/order.component';
-import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { DetailProductComponent } from './detail-product/detail-product.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { OrderComponent } from './components/order/order.component';
+import { OrderConfirmComponent } from './components/order-confirm/order-confirm.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DetailProductComponent } from './components/detail-product/detail-product.component';
 import { FormsModule } from '@angular/forms'; // import để sử dụng data binding trong form
-import { HttpClientModule } from '@angular/common/http'; // import để sử dụng http bên component
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; // import để sử dụng http bên component
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,11 @@ import { HttpClientModule } from '@angular/common/http'; // import để sử d�
     DetailProductComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true,
+  }],
   bootstrap: [LoginComponent],
 })
-export class AppModule {}
+export class AppModule { }
